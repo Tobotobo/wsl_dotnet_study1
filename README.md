73606923081cb20118749a77e7cc2501de93b1a0
Tobotobo <tobotobo.channel@gmail.com>
2026-05-27 01:24:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# wsl_dotnet_study1
+
+## 概要
+
+* WSL を使って .NET 10 の開発環境を構築する
+
+## 詳細
+
+### 目次
+* 環境構築
+* 起動
+* 削除
+* コンソールアプリ
+* ASP.NET Core
+
+### 環境構築
+```
+wsl --install AlmaLinux-10 --name AlmaLinux-10-Dotnet10 --no-launch
+wsl -u root -d AlmaLinux-10-Dotnet10 -- ./setup.sh
+wsl -t AlmaLinux-10-Dotnet10
+```
+
+### 起動
+```
+wsl -d AlmaLinux-10-Dotnet10
+```
+
+### 削除
+```
+wsl --unregister AlmaLinux-10-Dotnet10
+```
+
+### コンソールアプリ
+```
+cd ~
+mkdir ConsoleApp1
+cd ./ConsoleApp1 
+dotnet new console
+dotnet run
+```
+
+結果
+```
+$ dotnet run
+Hello, World!
+```
+
+### ASP.NET Core
+```
+cd ~
+mkdir ./AspDotnetCore1
+cd ./AspDotnetCore1
+dotnet new web
+dotnet run
+```
+
+```
+$ dotnet run
+Using launch settings from /home/dev/AspDotnetCore1/Properties/launchSettings.json...
+Building...
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: http://localhost:5185
+info: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+info: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Development
+info: Microsoft.Hosting.Lifetime[0]
+      Content root path: /home/dev/AspDotnetCore1
+```
+
+![alt text](docs/images/image.png)
---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# 途中でエラーが発生した場合、スクリプトを即座に終了する
+set -e
+
+# root権限（sudo）で実行されているか確認
+if [ "$EUID" -ne 0 ]; then
+  echo "エラー: このスクリプトは root 権限で実行する必要があります。sudo を付けて実行してください。" >&2
+  exit 1
+fi
+
+echo "WSL の DNS 設定ファイル (/etc/wsl.conf) の自動生成を無効化中..."
+cat << 'EOF' >> /etc/wsl.conf
+[network]
+generateResolvConf = false
+EOF
+
+echo "WSL の DNS 設定ファイル (/etc/resolv.conf) を作成中..."
+rm -f /etc/resolv.conf
+cat << 'EOF' > /etc/resolv.conf
+nameserver 8.8.8.8
+EOF
+
+# echo "AlmaLinux をアップデート..."
+# dnf update -y
+# dnf upgrade -y
+
+echo "日本語言語パックをインストール..."
+dnf install -y glibc-langpack-ja
+
+echo "タイムゾーンを Asia/Tokyo に設定..."
+timedatectl set-timezone Asia/Tokyo
+
+echo "ロケールを ja_JP.UTF-8 に設定..."
+localectl set-locale LANG=ja_JP.UTF-8
+
+echo "ユーザー dev を作成中..."
+useradd -m -s /bin/bash dev
+
+echo "ユーザー dev に sudo 権限を設定中..."
+echo "dev ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/dev
+chmod 0440 /etc/sudoers.d/dev
+
+echo "WSL のデフォルトログインユーザーをユーザー dev に設定中..."
+cat << 'EOF' >> /etc/wsl.conf
+[user]
+default=dev
+EOF
+
+echo "WSL のデフォルトユーザー作成機能を無効化中..."
+sed -i -E 's|^[[:space:]]*command[[:space:]]*=[[:space:]]*/usr/lib/wsl/oobe[[:space:]]*$|# command = /usr/lib/wsl/oobe|' "/etc/wsl-distribution.conf"
+
+echo "一般ユーザーが ping コマンドを使用できるように設定中..."
+mkdir -p /etc/sysctl.d
+tee /etc/sysctl.d/99-ping.conf >/dev/null <<'EOF'
+net.ipv4.ping_group_range = 0 2147483647
+EOF
+
+echo "git をインストール中..."
+dnf install -y git
+git --version
+
+echo "dotnet-sdk-10.0 をインストール中..."
+dnf install -y dotnet-sdk-10.0
+dotnet --version
+
+echo "設定完了"
